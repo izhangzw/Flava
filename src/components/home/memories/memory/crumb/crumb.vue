30668144fc3ddb4319f0d08dcad20b59902f6df7
zhangzw <imlovinitz@163.com>
2021-09-01 22:57:04 +0800

    updated templates

---
+++ src/components/home/memories/memory/crumb/crumb.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="crumb">
+    <i v-for="(crumb, index) in crumbs" :key="index" :class="'crumb-' + crumb"></i>
+  </div>
+</template>
+
+<script>
+import '@/assets/styles/base/variable.scss'
+export default {
+  name: 'crumb',
+  computed: {
+    crumbs () {
+      return ['photo', 'video', 'weblink']
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.crumb {
+  position: absolute;
+  > i {
+    width: 4px;
+    height: 4px;
+    border-radius: 50%;
+    display: inline-block;
+    margin: 1px;
+    &.crumb- {
+      &photo {
+        background-color: $photoColor;
+      }
+      &video {
+        background-color: $videoColor;
+      }
+      &weblink {
+        background-color: $weblinkColor;
+      }
+    }
+  }
+}
+</style>
--- src/components/home/memories/memory/memory.vue
+++ src/components/home/memories/memory/memory.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-      <h1>{{item.title}}</h1>
-      <h3>{{item.subTitle}}</h3>
+  <div class="memory place">
+    <crumb></crumb>
+    <div class="memory-content">
+      <div class="memory-content-infor">
+        <p class="memory-content-infor-captain">Greenmonster Inc.</p>
+        <p class="memory-content-infor-desc">#666 Gsan-dong, Greenmonster</p>
+      </div>
+      <div class="memory-content-media"></div>
+    </div>
+    <div class="memory-timeline"></div>
   </div>
 </template>
 
 <script>
+import crumb from '@/components/home/memories/memory/crumb/crumb'
 export default {
   name: 'memory',
+  components: { crumb },
   props: {
     memory: Object
   },
@@ -17,35 +26,73 @@ export default {
     }
   },
   methods: {
-    onBackClick () {
-      this.$router.go(-1)
-    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.header {
-  position: fixed;
-  top: env(safe-area-inset-top);
-  left: 0;
-  right: 0;
-  background-color: aliceblue;
-}
+<style lang="scss" scoped>
+.memory {
+  position: relative;
+  margin-bottom: 1rem;
+  @include flexbox;
+  @include flex-direction(row);
 
-.header-menu {
-  position: absolute;
-  left: 0;
-  width: 44px;
-  height: 100%;
-  background-color: aqua;
+  &-content {
+    @include flex(1);
+    padding: 0;
+    border-bottom: 1px solid #f0f0f0;
+
+    @include flexbox;
+    @include flex-direction(row);
+    &-infor {
+      @include flex(1);
+      padding-right: 1rem;
+
+      @include flexbox;
+      @include flex-direction(column);
+      > p {
+        @include flex(1);
+        text-align: right;
+      }
+      &-captain {
+        @include font-size(1.6);
+        color: $titleColor;
+        @include flexbox;
+        @include flex-direction(column);
+        @include justify-content(flex-end);
+      }
+      &-desc {
+        @include font-size(1.4);
+        color: $descColor;
+      }
+    }
+    &-media {
+      background: red;
+    }
+  }
+
+  &-timeline {
+    width: 4px;
+    height: auto;
+    display: block;
+    background-color: darkblue;
+    margin-right: 1rem;
+    margin-left: 1rem;
+  }
+
+  .crumb {
+    left: 1rem;
+  }
 }
-.header-create {
-  position: absolute;
-  right: 0;
-  width: 44px;
-  height: 100%;
-  background-color: yellowgreen;
+.memory.place {
+  .memory-content-media {
+    width: 6rem;
+    height: 6rem;
+    background-color: $placeColor;
+    &::after {
+      content: 'Place';
+    }
+  }
 }
 </style>
