ea1127a043bda662028a1a44d15b7e560d15a3e0
zhangzw <imlovinitz@163.com>
2021-09-04 17:46:34 +0800

    record page / router

---
+++ src/components/record/record.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="view-record">
+    <navigator
+    title="Record New / Edit"
+    left-icon-class="fa-close"
+    :left-icon-action="onCancelButtonClick"
+    :show-right-navigation="true"
+    right-icon-class="fa-check"
+    :right-icon-action="onSubmitButtonClick"
+    ></navigator>
+    <section class="bodyer">
+      <record-datetime></record-datetime>
+      <record-editable></record-editable>
+      <record-footer></record-footer>
+    </section>
+  </div>
+</template>
+
+<script>
+import navigator from '@/components/navigator/navigator'
+
+export default {
+  name: 'record',
+  components: { navigator },
+  methods: {
+    onCancelButtonClick () {
+      console.log('点击取消按钮')
+      this.$router.go(-1)
+    },
+    onSubmitButtonClick () {
+      console.log('点击提交按钮')
+      this.$router.go(-1)
+    }
+  },
+  created () {
+    console.log('created  init')
+  },
+  mounted () {
+    console.log('mounted call everytime')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home/home'
+import record from '@/components/record/record'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
+    },
+    {
+      path: '/record',
+      name: 'record',
+      component: record
     }
   ]
 })
