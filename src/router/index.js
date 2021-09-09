a53cc2ff2656b02d50a2d06ba0dd7c64b0263ab6
zhangzw <imlovinitz@163.com>
2021-09-10 00:57:18 +0800

    lazy load router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '@/components/home/home'
-import record from '@/components/record/record'
 
 Vue.use(Router)
 
@@ -10,12 +8,12 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: home
+      component: () => import('@/components/home/home')
     },
     {
       path: '/record',
       name: 'record',
-      component: record
+      component: () => import('@/components/record/record')
     }
   ]
 })
