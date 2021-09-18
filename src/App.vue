8dfb6ea6e5af2b97a84d57cf9f43d158c05ce418
zhangzw <imlovinitz@163.com>
2021-09-18 21:50:40 +0800

    updated common scss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container" id="app">
     <shelf></shelf>
-    <router-view/>
+    <router-view class="view" />
   </div>
 </template>
 
@@ -14,8 +14,20 @@ export default {
 </script>
 <style lang="scss">
 @import '@/assets/app.scss';
-
+$bodyer-margin-top: 44px;
 .container {
   height: 100%;
+  .view {
+    background-color: $globalViewBackgroundColor;
+  }
+}
+section.bodyer {
+  height: 100%;
+  width: 100%;
+  margin-top: calc( #{$bodyer-margin-top} + #{$safe-area-inset-top} );
+  background-color: inherit;
+}
+header.navigator {
+  background-color: inherit;
 }
 </style>
